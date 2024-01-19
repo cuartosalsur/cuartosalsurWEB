ac7abde3bf9cb593eb6506631db7a1ac2fa40ec6
Agustina Witis <agustina.witis@gmail.com>
2024-01-19 10:38:13 -0300

    Nosotros

--- src/components/body/nosotros/Nosotros.jsx
+++ src/components/body/nosotros/Nosotros.jsx
@@ -1,4 +1,5 @@
-import { Typography, Grid } from '@mui/material';
+import { Typography, Grid, Box } from '@mui/material';
+import {Wifi, Weekend, LocalParking, SupervisorAccount} from '@mui/icons-material';
 //logo
 import Logo from '../../../assets/img/logo2.png';
 
@@ -21,39 +22,57 @@ export const Nosotros = ({
           bgcolor: '#FDF2D9'
         }}
       >
-
-      <Grid 
-      container 
-      justifyContent={'center'}
-      alignItems={'center'}
-      >
-        <Grid item xs={12} sm={6}>
-        <Typography
-          sx={{
-            fontFamily: 'Kalam',
-            fontSize: {xs: '35px', lg: '50px'},
-            fontWeight: '500',
-            color: 'primary.main',
-            marginBottom: {xs: '10%', md: '2%'},
-            textAlign: 'center'
-          }}
-        >{titulo}</Typography>
-          <Typography
-            sx={{
-              textAlign: 'center',
-              fontSize: {xs: '18px', lg: '22px'},
-              color: 'secondary.main',
-            }}
-          >
-            {p}
-          </Typography>
-        </Grid>
-            
-        <Grid item xs={12} sm={6} sx={{marginTop: {xs: '20px', sm: 0}, justifyContent: 'center'}}>
-            <img src={Logo} className='logo'/>          
-        </Grid>
-
-      </Grid>
+        <Grid 
+        container 
+        justifyContent={'center'}
+        alignItems={'center'}
+        spacing={10}
+        >
+            <Grid item xs={12} sm={7}>
+            <Typography
+              sx={{
+                fontFamily: 'Kalam',
+                fontSize: {xs: '35px', lg: '50px'},
+                fontWeight: '500',
+                color: 'primary.main',
+                marginBottom: {xs: '10%', md: '2%'},
+                textAlign: 'center'
+              }}
+            >{titulo}</Typography>
+              <Typography
+                sx={{
+                  textAlign: 'center',
+                  fontSize: {xs: '18px', lg: '22px'},
+                  color: 'secondary.main',
+                }}
+              >
+                {p}
+              </Typography>
+            </Grid>
+                
+            <Grid item xs={12} sm={5} sx={{marginTop: {xs: '20px', sm: 0}, display: 'flex', justifyContent: 'center', gap: 5}}>
+              <Grid>
+                <Box sx={{width: '8em', height: '8em', backgroundColor: '#fff', border: '1px solid #424242', marginBottom: 5, display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+                  <Wifi fontSize="large"/>
+                  <Typography>Free Wifi</Typography>
+                </Box>
+                <Box sx={{width: '8em', height: '8em', backgroundColor: '#fffe', border: '1px solid #424242', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+                <Weekend fontSize="large"/>
+                  <Typography>Fully Furnished</Typography>
+                </Box>
+              </Grid>
+              <Grid>
+                <Box sx={{width: '8em', height: '8em', backgroundColor: '#fff', border: '1px solid #424242', marginBottom: 5, display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+                <SupervisorAccount fontSize="large"/>
+                  <Typography>Admin Service</Typography>
+                </Box>
+                <Box sx={{width: '8em', height: '8em', backgroundColor: '#fffe', border: '1px solid #424242', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+                <LocalParking fontSize="large"/>
+                  <Typography>Free Parking</Typography>
+                </Box>
+              </Grid>
+            </Grid>
+          </Grid>
       </Grid>
     </>
   );
